9d9e2a0c48c3949804cabdad0e05ecfd2f388c23
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-25 18:44:33 -0300

    Creating Form and connecting to the API

--- receipes-app/src/App.js
+++ receipes-app/src/App.js
@@ -1,23 +1,32 @@
 import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form';
 
-function App() {
-  return (
+const API_KEY = "1c446efe406b4a3da0e20cce7acb07cc";
+
+
+class App extends Component {
+  //Will make the API calls
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+
+    const apiCall = await fetch(`https://api.spoonacular.com/recipes/complexSearch/?apiKey=${API_KEY}`);
+    const data = await apiCall.json();
+    console.log(data.results); // You can see it in console when you press the buttom
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">        
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Recipe Search</h1>
       </header>
+      <Form getRecipe={this.getRecipe} />
     </div>
   );
+  }
+  
 }
 
 export default App;
