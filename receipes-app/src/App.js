ca4be3afae36acc3dd7a645d184b08d81e32c6bc
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-26 08:40:21 -0300

    Making searchbar functional

--- receipes-app/src/App.js
+++ receipes-app/src/App.js
@@ -6,15 +6,21 @@ const API_KEY = "1c446efe406b4a3da0e20cce7acb07cc";
 
 
 class App extends Component {
+  state = {
+      recipes: [],
+  }
+
   //Will make the API calls
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
 
-    const apiCall = await fetch(`https://api.spoonacular.com/recipes/complexSearch/?apiKey=${API_KEY}`);
+    const apiCall = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${recipeName}`);
     const data = await apiCall.json();
-    console.log(data.results); // You can see it in console when you press the buttom
+    this.setState({recipes: data.results});
+    console.log(this.state.recipes)
   }
+  
 
   render() {
     return (
@@ -23,6 +29,9 @@ class App extends Component {
         <h1 className="App-title">Recipe Search</h1>
       </header>
       <Form getRecipe={this.getRecipe} />
+      { this.state.recipes.map((recipe) => {
+        return <p key={recipe.id}>{recipe.title}</p>;
+      }) }
     </div>
   );
   }
